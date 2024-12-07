13d331c936b33d82b53d5079c441e913fc30444f
anisanhy <joyabrianrantepasang14@gmail.com>
2024-12-07 22:07:46 +0800

    menu scene

---
+++ Assets/Script/UI/Menu/SocialMediaButton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocialMediaButton : MonoBehaviour
+{
+    public void OpenInstagram()
+    {
+        Application.OpenURL("https://www.instagram.com/joyrantepasang");
+    }
+
+    public void OpenYoutube()
+    {
+        Application.OpenURL("https://www.youtube.com/@joyabrianrantepasang815");
+    }
+}
